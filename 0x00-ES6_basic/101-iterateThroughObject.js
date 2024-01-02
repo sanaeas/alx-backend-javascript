47746b9da614403e8104e763f82be6d9599a6b8e
sanaeas <sanaassim1@gmail.com>
2024-01-02 18:22:16 +0100

    Iterate through object

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,9 @@
+export default function iterateThroughObject(reportWithIterator) {
+  let employees = '';
+
+  for (const employee of reportWithIterator) {
+    employees += `${employee} | `;
+  }
+
+  return employees.slice(0, -3);
+}
