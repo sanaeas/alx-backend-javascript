1ff9c109a8787318f695b9307c6368f40c3fa92b
sanaeas <sanaassim1@gmail.com>
2024-01-03 12:10:49 +0100

    Fix: add return

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,10 +4,10 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => {
+    .then((results) => (
       results.map((result) => ({
         status: result.status,
         value: result.status === 'fulfilled' ? result.value : String(result.reason),
-      }));
-    });
+      }))
+    ));
 }
