c1997c1246139d8840f9a599600bf241b16d3f74
sanaeas <sanaassim1@gmail.com>
2024-01-04 10:18:10 +0100

    HolbertonCourse Class

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,40 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(val) {
+    if (typeof val !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = val;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(val) {
+    if (typeof val !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = val;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(val) {
+    if (!Array.isArray(val) || !val.every((item) => typeof item === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = val;
+  }
+}
