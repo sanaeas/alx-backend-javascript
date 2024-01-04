a09cf923a10a71c8e03557a5c31df6c747ec3a18
sanaeas <sanaassim1@gmail.com>
2024-01-04 10:27:30 +0100

    Implementing a class method

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,32 @@
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(val) {
+    if (typeof val !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = val;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(val) {
+    if (typeof val !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = val;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
