3b6656a1f7411c4a30e10d539f2f7c514ba32386
sanaeas <sanaassim1@gmail.com>
2024-01-08 18:21:04 +0100

    Fix: Weak link data structure

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,11 +1,12 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  const count = weakMap.get(endpoint) || 0;
+  let count = weakMap.get(endpoint) || 0;
+
+  weakMap.set(endpoint, count + 1);
+  count += 1;
 
   if (count >= 5) {
     throw new Error('Endpoint load is high');
   }
-
-  weakMap.set(endpoint, count + 1);
 }
