94f720449790d086624d827d0720af4e23a29427
sanaeas <sanaassim1@gmail.com>
2024-02-27 11:02:47 +0100

    Fix lint

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -7,34 +7,30 @@ function countStudents(path) {
 
     let csCount = 0;
     let sweCount = 0;
-    let csList = [];
-    let sweList = [];
+    const csList = [];
+    const sweList = [];
 
     for (const line of lines) {
       const student = line.split(',');
       if (
-        student.length === 4 &&
-        student[0] !== '' &&
-        student[1] !== '' &&
-        student[3] !== ''
+        student.length === 4
+        && student[0] !== ''
+        && student[1] !== ''
+        && student[3] !== ''
       ) {
         if (student[3] === 'CS') {
-          csCount++;
+          csCount += 1;
           csList.push(student[0]);
         } else if (student[3] === 'SWE') {
-          sweCount++;
+          sweCount += 1;
           sweList.push(student[0]);
         }
       }
     }
 
     console.log(`Number of students: ${csCount + sweCount}`);
-    console.log(
-      `Number of students in CS: ${csCount}. List: ${csList.join(', ')}`
-    );
-    console.log(
-      `Number of students in SWE: ${sweCount}. List: ${sweList.join(', ')}`
-    );
+    console.log(`Number of students in CS: ${csCount}. List: ${csList.join(', ')}`);
+    console.log(`Number of students in SWE: ${sweCount}. List: ${sweList.join(', ')}`);
   } catch (error) {
     throw new Error('Cannot load the database');
   }
