f5ac843ddaf9b57ce35edc084273f132bf48e636
sanaeas <sanaassim1@gmail.com>
2024-02-27 13:06:28 +0100

    Organize a complex HTTP server using Express

---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,36 @@
+import fs from 'fs';
+
+function getStudents(data) {
+  const lines = data.split('\n');
+  const students = [];
+  for (let i = 1; i < lines.length; i += 1) {
+    if (lines[i] !== '') {
+      students.push(lines[i]);
+    }
+  }
+  return students;
+}
+
+function countFields(students) {
+  const fields = {};
+  let info;
+  let name;
+  let field;
+  students.forEach((student) => {
+    [name, ...info] = student.split(',');
+    field = info[info.length - 1];
+    if (!(field in fields)) {
+      fields[field] = [];
+    }
+    fields[field].push(name);
+  });
+  return fields;
+}
+
+async function readDatabase(path) {
+  const data = await fs.promises.readFile(path, { encoding: 'utf-8' });
+  const students = getStudents(data);
+  return countFields(students);
+}
+
+export default readDatabase;
