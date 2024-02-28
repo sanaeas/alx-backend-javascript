9d65d58b27e86c70fb586b6313be1dd2dbf5aa93
sanaeas <sanaassim1@gmail.com>
2024-02-28 19:10:30 +0100

    Fix

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -5,18 +5,17 @@ const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', () => {
   it('call console.log with the correct arguments', () => {
-    const calculateNumberStub = sinon
-      .stub(Utils, 'calculateNumber')
-      .returns(10);
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
     const consoleSpy = sinon.spy(console);
+    calculateNumberStub.returns(10);
 
     sendPaymentRequestToApi(100, 20);
 
     expect(calculateNumberStub.calledOnce).to.be.true;
-    expect(calculateNumberStub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true;
 
     expect(consoleSpy.log.calledOnce).to.be.true;
-    expect(consoleSpy.log.calledOnceWithExactly('The total is: 10')).to.be.true;
+    expect(consoleSpy.log.calledWith('The total is: 10')).to.be.true;
 
     calculateNumberStub.restore();
     consoleSpy.log.restore();
