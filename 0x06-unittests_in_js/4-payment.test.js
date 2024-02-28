99ce2c015fa34b76720c39703f4bd740a4f7a68a
sanaeas <sanaassim1@gmail.com>
2024-02-28 19:01:50 +0100

    Stubs

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -4,21 +4,21 @@ const sendPaymentRequestToApi = require('./4-payment');
 const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', () => {
-  it('call Utils.calculateNumber with the correct arguments', () => {
+  it('call console.log with the correct arguments', () => {
     const calculateNumberStub = sinon
       .stub(Utils, 'calculateNumber')
       .returns(10);
-    const consoleSpy = sinon.spy(console, 'log');
+    const consoleSpy = sinon.spy(console);
 
     sendPaymentRequestToApi(100, 20);
 
     expect(calculateNumberStub.calledOnce).to.be.true;
-    expect(calculateNumberStub.calledWithExactly('SUM', 100, 20)).to.be.true;
+    expect(calculateNumberStub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
 
-    expect(consoleSpy.calledOnce).to.be.true;
-    expect(consoleSpy.calledWithExactly('The total is: 10')).to.be.true;
+    expect(consoleSpy.log.calledOnce).to.be.true;
+    expect(consoleSpy.log.calledOnceWithExactly('The total is: 10')).to.be.true;
 
     calculateNumberStub.restore();
-    consoleSpy.restore();
+    consoleSpy.log.restore();
   });
 });
