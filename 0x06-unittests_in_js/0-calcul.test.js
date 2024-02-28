d57710bde615e68b5d0b16b68329fb5f2b58c3dc
sanaeas <sanaassim1@gmail.com>
2024-02-28 17:52:38 +0100

    Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -10,11 +10,23 @@ describe('calculateNumber', function () {
     assert.strictEqual(calculateNumber(1, 2.7), 4);
   });
 
+  it('sum of rounded numbers when one is float with .0', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.4), 3);
+  });
+
+  it('sum of rounded numbers when both inputs are floats under .5', () => {
+    assert.strictEqual(calculateNumber(2.2, 3.4), 5);
+  });
+
   it('sum of rounded numbers when both inputs are floats', () => {
     assert.strictEqual(calculateNumber(2.2, 3.7), 6);
   });
 
   it('sum of rounded numbers when both inputs are floats with .5 rounding', () => {
     assert.strictEqual(calculateNumber(3.5, 3.7), 8);
+  });
+
+  it('sum of rounded negative numbers when both inputs are floats', () => {
+    assert.strictEqual(calculateNumber(-2.6, -2.0), -5);
   });
 });
